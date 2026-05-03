055df4e8093791ed6382c9351a61af7b430144f3
Antony <antonymarshan.qa@gmail.com>
2026-05-03 23:35:27 +0530

    testng annotation added

--- src/test/java/basic/BasicTest.java
+++ src/test/java/basic/BasicTest.java
@@ -2,15 +2,17 @@ package basic;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class BasicTest {
 
-	public static void main(String[] args) {
+	@Test
+	public void testCase01() {
 		System.out.println("---------- Testcase started..!!! ------------");
-		
+
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.selenium.dev/downloads/");
-		
+
 		System.out.println("---------- Testcase finished..!!! ------------");
 	}
 
