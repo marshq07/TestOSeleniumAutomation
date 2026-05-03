54eb1b84933d3afced454bff8c6a4ed6ff31f9b6
Antony <antonymarshan.qa@gmail.com>
2026-05-03 23:20:37 +0530

    added webdriver

--- src/test/java/basic/BasicTest.java
+++ src/test/java/basic/BasicTest.java
@@ -1,9 +1,16 @@
 package basic;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BasicTest {
 
 	public static void main(String[] args) {
 		System.out.println("---------- Testcase started..!!! ------------");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.selenium.dev/downloads/");
+		
 		System.out.println("---------- Testcase finished..!!! ------------");
 	}
 
