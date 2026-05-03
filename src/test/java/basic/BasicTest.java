6d60c8278de7591177ca2f53e767a7632100eac6
Antony <antonymarshan.qa@gmail.com>
2026-05-03 21:22:59 +0530

    Created BaseTest

---
+++ src/test/java/basic/BasicTest.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class BasicTest {
+
+	public static void main(String[] args) {
+		System.out.println("---------- Testcase started..!!! ------------");
+		System.out.println("---------- Testcase finished..!!! ------------");
+	}
+
+}
